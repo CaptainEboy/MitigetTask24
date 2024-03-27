212c2d5b53d2e6de844a3fe04b14e364b2cfbd2d
CaptainEboy <emaapps.ac24@gmail.com>
2024-03-27 14:45:53 +0100

    First Commit

---
+++ env/hashing.py
@@ -0,0 +1,20 @@
+# from passlib.context import CryptContext
+
+# pwd_cxt = CryptContext(schemes =["bcrypt"],deprecated="auto")
+
+# class Hash():
+#    def bcrypt(password:str):
+#       return pwd_cxt.hash(password)
+#    def verify(hashed,normal):
+#       return pwd_cxt.verify(normal,hashed)
+
+
+from passlib.context import CryptContext
+
+pwd_cxt = CryptContext(schemes =["bcrypt"],deprecated="auto")
+
+class Hash():
+	def bcrypt(password:str):
+		return pwd_cxt.hash(password)
+	def verify(hashed,normal):
+		return pwd_cxt.verify(normal,hashed)
